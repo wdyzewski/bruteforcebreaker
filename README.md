ea07e291d23663ad7363f3657028f76737eb8701
ArthurHoaro <arthur@hoa.ro>
2013-08-10 12:23:10 +0200

    1.0 initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Roundcube - Bruteforce Breaker
+
+This plugin will ban IP address for X minutes after several consecutive failed login attemps.
+
+When an IP is banned, RoundCube will return invalid credentials.
+
+## Requirements
+
+There is no dependency. This plugin was developped for Roundcube 0.9.0+ but it might be compatible with earlier versions.
+
+## Installation
+
+ * Upload the plugin in your RoundCube installation into `/plugins/`.
+ * Rename the plugin folder into `bruteforcebreaker`.
+ * Add the plugin in your plugins array in `/config/main.inc.php` :
+ 
+```php
+$rcmail_config['plugins'] = array('bruteforcebreaker');
+```
+ * It's already working ! :)
+
+## Configuration
+
+You can override default settings by copying `config.inc.php.dist` into `config.inc.php`.
+
+ * keep_trace: Log login attemps.
+ * nb_attemps: Number of login attemps before ban
+ * duration: Ban duration in seconds
+
+## License
+
+This software is distributed under [the MIT license](http://git.hoa.ro/arthur/rc-plugin-bruteforce-breaker/blob/master/) by Arthur Hoaro.
+
+Thanks to [SebSauvage](https://github.com/sebsauvage/) for the inspiration.
---
+++ bruteforcebreaker.php
@@ -0,0 +1,91 @@
+<?php
+/**
+* RoundCube BruteforceBreaker Plugin
+*
+* Several consecutive failed logins will ban the IP address for 30 minutes.
+*
+* @version 1.0
+* @author Arthur Hoaro <http://hoa.ro>
+* @url http://git.hoa.ro/arthur/rc-plugin-bruteforce-breaker/tree/master/
+* @license GPLv3
+*/
+class bruteforcebreaker extends rcube_plugin {
+    private $rc;
+    private $ipbans_file;
+    private $data_ban = array('FAILURES'=>array(),'BANS'=>array());
+
+    function init(){
+        $this->rc = rcmail::get_instance();
+        $this->ipbans_file = $this->home.'/ipbans.php';
+        
+        $this->add_hook('login_failed', array($this, 'ban_loginFailed'));
+        $this->add_hook('login_after', array($this, 'ban_loginOk'));
+        $this->add_hook('authenticate', array($this, 'ban_canLogin'));
+    }
+
+    function load_ipban() {
+        $this->load_config();
+        if (!is_file($this->ipbans_file)) $this->write_ipban();
+        include $this->ipbans_file;
+    }
+
+    function write_ipban() {
+        file_put_contents($this->ipbans_file, "<?php\n\$this->data_ban=".var_export( $this->data_ban,true ).";\n?>");
+    }
+
+    // Signal a failed login. Will ban the IP if too many failures:
+    function ban_loginFailed($args) {
+        $this->load_ipban();
+        $ip = $_SERVER['REMOTE_ADDR']; 
+
+        if (!isset($this->data_ban['FAILURES'][$ip])) 
+          $this->data_ban['FAILURES'][$ip] = 0;
+        $this->data_ban['FAILURES'][$ip]++;
+        if ($this->rc->config->get('bruteforcebreaker_keep_trace')) 
+            write_log('bruteforcebreaker', sprintf("Login failed for %s. Number of attemps: %d.\n", $ip, $this->data_ban['FAILURES'][$ip]));
+         
+        if ($this->data_ban['FAILURES'][$ip] > ($this->rc->config->get('bruteforcebreaker_nb_attemps') -1 )) {
+            $this->data_ban['BANS'][$ip] = time() + $this->rc->config->get('bruteforcebreaker_duration');
+            if ($this->rc->config->get('bruteforcebreaker_keep_trace')) 
+                write_log('bruteforcebreaker', sprintf("IP address banned from login - too many attemps (%s)\n", $ip));
+        }
+
+        $this->write_ipban();
+        return $args;
+    }
+
+    // Signals a successful login. Resets failed login counter.
+    function ban_loginOk($args) {
+        $this->load_ipban();
+        $ip = $_SERVER['REMOTE_ADDR'];
+        unset($this->data_ban['FAILURES'][$ip]); 
+        unset($this->data_ban['BANS'][$ip]);
+        
+        $this->write_ipban();
+        if ($this->rc->config->get('bruteforcebreaker_keep_trace')) 
+            write_log('bruteforcebreaker', sprintf("Login ok for %s.\n", $ip));
+        return $args;
+    }
+
+    // Checks if the user CAN login. If 'true', the user can try to login.
+    function ban_canLogin($args) {
+        $this->load_ipban();
+        $ip=$_SERVER["REMOTE_ADDR"]; 
+
+        if (!empty($this->data_ban['BANS'][$ip]) ) {
+            if( $this->data_ban['BANS'][$ip]<=time()) {             
+                unset($this->data_ban['FAILURES'][$ip]); 
+                unset($this->data_ban['BANS'][$ip]);
+                
+                $this->write_ipban();
+                if ($this->rc->config->get('bruteforcebreaker_keep_trace')) 
+                    write_log('bruteforcebreaker', sprintf("Ban lifted for %s.\n", $ip));
+            }
+            else $args['pass'] = '';
+        }      
+        
+        return $args; 
+    }
+}
+
+?>
